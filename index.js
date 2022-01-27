93fd423e4e212a303770a5a29a4073ca506befab
bora001 <bora.vancouver@gmail.com>
2022-01-26 22:31:34 -0800

    server setting

---
+++ index.js
@@ -0,0 +1,22 @@
+// server
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+// app.use(express.urlencoded());
+app.use(express.static(__dirname + "/client"));
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", process.env.localUrl + ":5500");
+  res.header("Access-Control-Allow-Credentials", true);
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, (req, res) => {
+  console.log("server is connected...");
+});
