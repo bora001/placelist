413aba8aa7a3a652250d2b2401162e271cc3b0e6
bora001 <bora.vancouver@gmail.com>
2022-01-30 00:35:39 +0900

    login data

--- client/index.js
+++ client/index.js
@@ -7,15 +7,21 @@ formBtn.forEach((btn) => {
   btn.addEventListener("click", (e) => {
     e.preventDefault();
 
-    if (e.target.innerText === "Register") {
-      registerForm();
+    switch (e.target.innerText) {
+      case "Register":
+        registerForm();
+        break;
+      case "Login":
+        loginForm();
+        break;
+      default:
+        break;
     }
   });
 });
 
 const registerForm = () => {
   console.log("register!");
-
   let pw;
   let data = [];
   for (let v of Object.values(formInput)) {
@@ -27,6 +33,16 @@ const registerForm = () => {
       alert("Incorrect Password");
       return;
     }
+  }
+  console.log(data);
+};
+
+const loginForm = () => {
+  let data = [];
+  for (let v of Object.values(formInput)) {
+    let obj = new Object();
+    obj[v.name] = v.value;
+    data.push(obj);
   }
   console.log(data);
 };
