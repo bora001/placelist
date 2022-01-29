fb46a748035aeb2a1e743a277dddd914af9c6488
bora001 <bora.vancouver@gmail.com>
2022-01-29 06:10:38 -0800

    register input arr/obj

--- client/index.js
+++ client/index.js
@@ -17,10 +17,17 @@ const registerForm = () => {
   console.log("register!");
 
   const arr = Array.from(formInput, (input) => [input.name, input.value]);
-
-  console.log(arr);
-
   if (arr[1] == arr[2]) {
     console.log("correct");
   }
+  console.log(arr);
+
+  let arr = [];
+  for (let v of Object.values(formInput)) {
+    console.log(v.name, v.value);
+    let obj = new Object();
+    obj[v.name] = v.value;
+    arr.push(obj);
+  }
+  console.log(arr);
 };
