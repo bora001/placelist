35e27c140c5705cc45a91563f0b0c87c723b5d1c
bora001 <bora.vancouver@gmail.com>
2022-02-09 11:47:33 +0900

    mapbox setting

--- client/index.js
+++ client/index.js
@@ -185,14 +185,28 @@ const getData = () => {
   })
     .then((res) => res.json())
     .then((data) => {
-      setMap(data.data);
+      let theData = data.data;
+      let collection = {
+        features: [],
+      };
+      theData.map((place) => {
+        let obj = {
+          geometry: place.geometry,
+          properties: {
+            price: place.price,
+            name: place.name,
+          },
+        };
+        collection.features.push(obj);
+      });
+      setMap(collection);
     })
     .catch((err) => console.log(err));
 };
 
 //map
-const setMap = (data) => {
-  console.log("data!", data);
+
+const setMap = (collection) => {
   mapboxgl.accessToken = key;
   const map = new mapboxgl.Map({
     container: "map",
@@ -200,8 +214,100 @@ const setMap = (data) => {
     center: [151.20776, -33.86854],
     zoom: 3,
   });
-};
 
+  console.log(collection);
+  map.on("load", () => {
+    map.addSource("placelist", {
+      type: "geojson",
+      data: collection,
+      cluster: true,
+      clusterMaxZoom: 14,
+      clusterRadius: 50,
+    });
+
+    map.addLayer({
+      id: "clusters",
+      type: "circle",
+      source: "placelist",
+      filter: ["has", "point_count"],
+      paint: {
+        "circle-color": [
+          "step",
+          ["get", "point_count"],
+          "#51bbd6",
+          100,
+          "#f1f075",
+          750,
+          "#f28cb1",
+        ],
+        "circle-radius": ["step", ["get", "point_count"], 20, 100, 30, 750, 40],
+      },
+    });
+
+    map.addLayer({
+      id: "cluster-count",
+      type: "symbol",
+      source: "placelist",
+      filter: ["has", "point_count"],
+      layout: {
+        "text-field": "{point_count_abbreviated}",
+        "text-font": ["DIN Offc Pro Medium", "Arial Unicode MS Bold"],
+        "text-size": 12,
+      },
+    });
+
+    map.addLayer({
+      id: "unclustered-point",
+      type: "circle",
+      source: "placelist",
+      filter: ["!", ["has", "point_count"]],
+      paint: {
+        "circle-color": "#11b4da",
+        "circle-radius": 4,
+        "circle-stroke-width": 1,
+        "circle-stroke-color": "#fff",
+      },
+    });
+
+    // inspect a cluster on click
+    map.on("click", "clusters", (e) => {
+      const features = map.queryRenderedFeatures(e.point, {
+        layers: ["clusters"],
+      });
+      const clusterId = features[0].properties.cluster_id;
+      map
+        .getSource("placelist")
+        .getClusterExpansionZoom(clusterId, (err, zoom) => {
+          if (err) return;
+
+          map.easeTo({
+            center: features[0].geometry.coordinates,
+            zoom: zoom,
+          });
+        });
+    });
+
+    map.on("click", "unclustered-point", (e) => {
+      const coordinates = e.features[0].geometry.coordinates.slice();
+      const name = e.features[0].properties.name;
+      const price = e.features[0].properties.price;
+      // while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+      //   coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+      // }
+      new mapboxgl.Popup()
+        .setLngLat(coordinates)
+        .setHTML(`name: ${name}<br>price: ${price}`)
+        .addTo(map);
+    });
+
+    map.on("mouseenter", "clusters", () => {
+      map.getCanvas().style.cursor = "pointer";
+    });
+    map.on("mouseleave", "clusters", () => {
+      map.getCanvas().style.cursor = "";
+    });
+  });
+};
 getData();
 formSubmit();
 loginCheck();
