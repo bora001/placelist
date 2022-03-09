9284b3f6e028ad22e36175e597b9c20685912e0e
bora001 <bora.vancouver@gmail.com>
2022-03-09 12:43:51 +0900

    heroku

--- client/js/place.js
+++ client/js/place.js
@@ -63,10 +63,10 @@ const renderItem = (data, key) => {
             <div class="intro_box">
               <h3>${data.name}</h3>
               <div class="rate_input">
-                <span>&#9733;&#9733;&#9733;&#9733;&#9733;</span>
-                <span class="filled"
+                <p>&#9733;&#9733;&#9733;&#9733;&#9733;</p>
+                <p class="filled"
                 style="width: ${average * 20}%"
-                >&#9733;&#9733;&#9733;&#9733;&#9733;</span>
+                >&#9733;&#9733;&#9733;&#9733;&#9733;</p>
               </div>
               <p class="current_rate">${average > 0 ? average : ""}</p>
             </div>
@@ -141,10 +141,10 @@ const renderReview = (data) => {
   data.map((comment) => {
     const html = `<div class="review_item">
               <div class="review_rate">
-                <span>&#9733;&#9733;&#9733;&#9733;&#9733;</span>
-                <span style="width:${
+                <p>&#9733;&#9733;&#9733;&#9733;&#9733;</p>
+                <p style="width:${
                   comment.rate * 20
-                }%"class="filled">&#9733;&#9733;&#9733;&#9733;&#9733;</span>
+                }%"class="filled">&#9733;&#9733;&#9733;&#9733;&#9733;</p>
               </div>
               <div class="review_txt">
                 <h3>${comment.username}</h3>
